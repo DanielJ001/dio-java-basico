5dd81fe972f3bc64c9cd2e5adbec1f57ca614ed3
Daniel <danieljunior.08@hotmail.com>
2024-08-13 11:55:50 -0300

    Criando package

---
+++ java-anatomia-classes/src/daniel/junior/exemplopackage/ExemploPackage.java
@@ -0,0 +1,9 @@
+package daniel.junior.exemplopackage;
+public class ExemploPackage {
+    public static void main(String[] args) {
+        
+        System.out.println("Olá mundo. Este é um exemplo de package.");
+
+    }
+    
+}
