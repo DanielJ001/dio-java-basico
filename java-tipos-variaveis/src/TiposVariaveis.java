a43891fd3346a69aee1d7f141bbd6ac31acae676
Daniel <danieljunior.08@hotmail.com>
2024-08-13 14:21:29 -0300

    Adição de algumas variaveis.

--- java-tipos-variaveis/src/TiposVariaveis.java
+++ java-tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,14 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        byte idade = 123;
+        short ano = 2024;
+        int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo
+        long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro tipo (é necessário terminar com o L)
+        float pi = 3.14F; // tem que colocar um F no final se não da erro
+        double salario = 1275.33;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; // casting
     }
 }
