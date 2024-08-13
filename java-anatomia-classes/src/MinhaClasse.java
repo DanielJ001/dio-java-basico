4334dbf58b828d88bb97e5138582ece5059a7e1b
Daniel <danieljunior.08@hotmail.com>
2024-08-13 11:19:44 -0300

    Atualização do código implementando novas variaveis e métodos.

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -1,7 +1,21 @@
 public class MinhaClasse {
     
     public static void main (String [] args) {
+
         System.out.println("Olá mundo");
+
+        String primeiroNome = "Daniel";
+        String segundoNome = "da Silva";
+        String terceiroNome = "Junior";
+
+        String nomeCompleto = nomeCompleto (primeiroNome, segundoNome, terceiroNome);
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome, String terceiroNome) {
+
+        return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome).concat(" ").concat(terceiroNome);
+
     }
 
 }
