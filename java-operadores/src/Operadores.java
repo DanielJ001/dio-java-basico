42ffb4a52db6441b1b12a1eaed1080f09a761e22
Daniel <danieljunior.08@hotmail.com>
2024-08-14 10:27:50 -0300

    Equals e operadores relacionais

--- java-operadores/src/Operadores.java
+++ java-operadores/src/Operadores.java
@@ -1,7 +1,7 @@
 public class Operadores {
     public static void main(String[] args) {
 
-        //operador de atribuição =
+        //OPERADOR DE ATRIBUIÇÃO =
         String nome = "Daniel";
         int idade = 23;
         double peso = 50.2;
@@ -9,7 +9,7 @@ public class Operadores {
         boolean doadorOrgao = false;
         //Date dataNascimento = new Date();
 
-        //operadores aritmeticos
+        //OPERADORES ARITMETICOS
         double soma = 10.5 + 15.7;
         int subtracao = 113 - 25;
         int multiplicacao = 20 * 7;
@@ -20,6 +20,7 @@ public class Operadores {
         String nomeCompleto = "LINGUAGEM" + "JAVA"; // a soma em uma String irá juntar os dois textos, também chamado de concatenação
             System.out.println(nomeCompleto);
 
+        //CONCATENAÇÃO
         //Utilizar Debug nesta parte
         String concatenacao ="?";
         concatenacao = 1+1+1+"1";
@@ -33,14 +34,14 @@ public class Operadores {
         concatenacao = "1"+(1+1+1);
             System.out.println(concatenacao); // começou como uma String, mas ao ter uma expressão em evidência o programa a resolveu e depois concatenou com a String
 
-        //operadores unários
+        //OPERADORES UNÁRIOS
         int numero = 5;
         numero = - numero; // transformar o numero em negativo
             System.out.println(numero);
         numero = numero * -1; // multiplicar por -1 para o número ficar positivo novamente
             System.out.println(numero);
 
-        //incrementação e decrementação
+        //INCREMENTAÇÃO E DECREMENTAÇÃO
         int incrementacao = 5;
         incrementacao++; // é a mesma coisa que numero = numero +1;
             System.out.println(incrementacao++); // o número não foi incrementado pois o ++ está depois da variável, então a impressão vai pegar o valor original. Ele só realizou a operação depois da impressão
@@ -48,20 +49,40 @@ public class Operadores {
             System.out.println(++incrementacao); // se colocar ++ antes da variável a impressão vai pegar o novo valor
             System.out.println(--incrementacao); // decrementação
 
-        //negação
+        //NEGAÇÃO
         boolean variavel = true;
             System.out.println(!variavel);
             System.out.println(variavel); // ela continua sendo true porque o valor não foi atribuido novamente
         variavel = !variavel; // é melhor inverter um valor do tipo booleano desta maneira
             System.out.println(variavel);
 
-        //ternário
+        //TERNÁRIO
         int a, b;
         a = 5;
         b = 6;
 
         String ternario = a==b ? "verdadeiro" : "falso";
             System.out.println(ternario);
+
+        //EQUALS E OPERADORES RELACIONAIS
+        String nomeUm = "Daniel";
+        String nomeDois = new String("Daniel");
+            System.out.println((nomeUm).equals(nomeDois)); // usar equals quando for objeto
+
+        int relacional1 = 1;
+        int relacional2 = 2;
+
+        boolean simNao = relacional1 == relacional2;
+
+        if(relacional1 < relacional2){
+            System.out.println("a nossa condição é verdadeira");
+        }
+
+            System.out.println("relacional1 é igual a relacional2? " + simNao);
+        simNao = relacional1 != relacional2;
+            System.out.println("relacional1 é diferente de relacional2? " + simNao);
+        simNao = relacional1 > relacional2;
+            System.out.println("relacional1 é maior que relacional2? " + simNao);
 
     }
 }
