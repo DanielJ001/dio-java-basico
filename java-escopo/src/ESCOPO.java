c74bdb6a4bfc030329413870395f1c8e53cd4ccf
Daniel <danieljunior.08@hotmail.com>
2024-08-14 15:01:32 -0300

    Exemplos de como funciona o escopo

--- java-escopo/src/ESCOPO.java
+++ java-escopo/src/ESCOPO.java
@@ -1,5 +1,32 @@
 public class ESCOPO {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World! Primeira aula sobre Escopos");
+
+    public class Conta {
+        //variavel da classe conta
+        double saldo = 10.0;
+
+        public void sacar(Double valor){
+            //variavel local de método
+            double novoSaldo = saldo - valor;
+        }
+        public void imprimirSaldo(){
+            //disponível em toda classe
+            System.out.println(saldo);
+            //somente o método sacar conhece esta variavel
+            System.out.println(novoSaldo);
+        }
+        
+        public double calcularDividaExponencial(){
+            //variável local de método
+            double valorParcela = 50.0;
+            double valorMontante = 0.0;
+            for(int x=1; x<=5; x++) {
+                double valorCalculado = valorParcela * x;
+                valorMontante = valorMontante + valorCalculado; 
+            }
+            //valor de fluxo
+            //x e valorCalculado nunca estarão disponíveis fora deste escopo
+            return valorMontante;
+        }
+
     }
 }
