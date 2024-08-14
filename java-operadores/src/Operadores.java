b89d631bdd4a00a48215f2794ec0b2409988a32e
Daniel <danieljunior.08@hotmail.com>
2024-08-14 09:59:48 -0300

    operador ternário

--- java-operadores/src/Operadores.java
+++ java-operadores/src/Operadores.java
@@ -18,7 +18,7 @@ public class Operadores {
         double resultado = (10 * 7) + (20 / 4);
 
         String nomeCompleto = "LINGUAGEM" + "JAVA"; // a soma em uma String irá juntar os dois textos, também chamado de concatenação
-        System.out.println(nomeCompleto);
+            System.out.println(nomeCompleto);
 
         //Utilizar Debug nesta parte
         String concatenacao ="?";
@@ -36,23 +36,32 @@ public class Operadores {
         //operadores unários
         int numero = 5;
         numero = - numero; // transformar o numero em negativo
-        System.out.println(numero);
+            System.out.println(numero);
         numero = numero * -1; // multiplicar por -1 para o número ficar positivo novamente
-        System.out.println(numero);
+            System.out.println(numero);
 
         //incrementação e decrementação
         int incrementacao = 5;
         incrementacao++; // é a mesma coisa que numero = numero +1;
-        System.out.println(incrementacao++); // o número não foi incrementado pois o ++ está depois da variável, então a impressão vai pegar o valor original. Ele só realizou a operação depois da impressão
-        System.out.println(incrementacao);
-        System.out.println(++incrementacao); // se colocar ++ antes da variável a impressão vai pegar o novo valor
-        System.out.println(--incrementacao); // decrementação
+            System.out.println(incrementacao++); // o número não foi incrementado pois o ++ está depois da variável, então a impressão vai pegar o valor original. Ele só realizou a operação depois da impressão
+            System.out.println(incrementacao);
+            System.out.println(++incrementacao); // se colocar ++ antes da variável a impressão vai pegar o novo valor
+            System.out.println(--incrementacao); // decrementação
 
         //negação
         boolean variavel = true;
-        System.out.println(!variavel);
-        System.out.println(variavel); // ela continua sendo true porque o valor não foi atribuido novamente
+            System.out.println(!variavel);
+            System.out.println(variavel); // ela continua sendo true porque o valor não foi atribuido novamente
         variavel = !variavel; // é melhor inverter um valor do tipo booleano desta maneira
-        System.out.println(variavel);
+            System.out.println(variavel);
+
+        //ternário
+        int a, b;
+        a = 5;
+        b = 6;
+
+        String ternario = a==b ? "verdadeiro" : "falso";
+            System.out.println(ternario);
+
     }
 }
