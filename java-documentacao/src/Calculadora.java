8944c0066076d8d8307fc5cbce7452f492277353
Daniel <danieljunior.08@hotmail.com>
2024-08-16 10:43:03 -0300

    Comentarios

---
+++ java-documentacao/src/Calculadora.java
@@ -0,0 +1,23 @@
+/**
+ * <h1>Calculadora</h1>
+ * A calculadora realiza operações matemáticas entre números inteiros
+ * <p>
+ * <b>Note:</b> Leia atentamente a documentação desta classe
+ * para desfrutar dos recursos oferecidos pelo autor.
+ * 
+ * @author Daniel da Silva Junior
+ * @version 1.0
+ * @since 16/08/2024
+ */
+
+public class Calculadora {
+    /**
+     * Este método é utilizado para somar dois números interios
+     * @param numeroUm esse é o primeiro parametro do método
+     * @param numeroDois este é o segundo parametro do método
+     * @return int o resultado deste método é a soma dos dois números
+     */
+    public int somar(int numeroUm, int numeroDois) {
+        return numeroUm + numeroDois;
+    }
+}
