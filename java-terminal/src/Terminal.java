2fe0c5c42aac58cf36e93ddbb8006e7b7dd9ee19
Daniel <danieljunior.08@hotmail.com>
2024-08-16 13:12:02 -0300

    Mudança no nome da classe

--- java-terminal/src/Terminal.java
+++ java-terminal/src/Terminal.java
@@ -1,4 +1,4 @@
-public class App {
+public class Terminal {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World! Este é o primeiro commit da aula sobre Terminal");
     }
