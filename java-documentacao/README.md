b3908a3f9acc61860dd4258911bff77a5aa42349
Daniel <danieljunior.08@hotmail.com>
2024-08-16 09:28:11 -0300

    Primeiro commit da aula de documentação

---
+++ java-documentacao/README.md
@@ -0,0 +1,5 @@
+## Documentação
+
+🤖Este repositório é referente a aula de Documentação.
+
+    👀Um breve resumo sobre o que eu aprendi neste repositório: 
---
+++ java-documentacao/src/Documentacao.java
@@ -0,0 +1,5 @@
+public class Documentacao {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World! Primeiro commit da aula de documentação!");
+    }
+}
