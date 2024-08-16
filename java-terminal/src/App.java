15fb959d01b1940114c7ecb16c70c5ecfc985eef
Daniel <danieljunior.08@hotmail.com>
2024-08-16 13:10:41 -0300

    Primeiro commit da aula sobre Terminal

---
+++ java-terminal/src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World! Este é o primeiro commit da aula sobre Terminal");
+    }
+}
