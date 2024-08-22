f452824f5e8107f4e1e40b57c78420c3f5ea95f8
Daniel <danieljunior.08@hotmail.com>
2024-08-22 10:35:31 -0300

    Estrutura condicional simples

---
+++ java-controle-fluxo/README.md
@@ -0,0 +1,5 @@
+## Controle de Fluxo
+
+🤖Este repositório é referente a aula de Controle de Fluxo
+
+    👀Um breve resumo sobre o que eu aprendi neste repositório: 
--- java-estruturas-condicionais/src/EstruturaCondicionalSimples.java
+++
@@ -1,5 +0,0 @@
-public class EstruturaCondicionalSimples {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World! Primeiro commit da aula de Estrutura Condicional Simples");
-    }
-}
