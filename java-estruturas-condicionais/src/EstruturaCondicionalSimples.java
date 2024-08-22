2a54130c87fd68aac9b4d85cfc477c5b4f050fe1
Daniel <danieljunior.08@hotmail.com>
2024-08-22 10:00:28 -0300

    Estrutura condicional simples

---
+++ java-estruturas-condicionais/src/EstruturaCondicionalSimples.java
@@ -0,0 +1,5 @@
+public class EstruturaCondicionalSimples {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World! Primeiro commit da aula de Estrutura Condicional Simples");
+    }
+}
