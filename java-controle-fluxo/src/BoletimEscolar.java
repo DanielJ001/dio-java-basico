2908d40efa04383e6076eab02e5fc600280d7c1a
Daniel <danieljunior.08@hotmail.com>
2024-08-22 11:32:04 -0300

    Estrutura condicional encadeada + operador ternário

--- java-controle-fluxo/src/BoletimEscolar.java
+++ java-controle-fluxo/src/BoletimEscolar.java
@@ -10,5 +10,11 @@ public class BoletimEscolar {
 
         else
             System.out.println("Reprovado");
+        
+        // Utilizando operador ternário
+
+        String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "Recuperação" : "Reprovado";
+            
+        System.out.println(resultado);
     }
 }
